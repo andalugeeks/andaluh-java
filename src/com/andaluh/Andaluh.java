11a7a59f97f38279031e4a6d4f105adfdb266121
arrivero <angel.rodriguez.rivero@gmail.com>
2020-03-04 23:56:47 +0100

    Adds dummy transliterate function, adds escapeLinks logic, proposes word-ignoring data structure

--- src/com/andaluh/Andaluh.java
+++ src/com/andaluh/Andaluh.java
@@ -2,6 +2,21 @@ package com.andaluh;
 import static java.nio.charset.StandardCharsets.*;
 
 public class Andaluh {
+    public static String transliterate(String text) {
+        /*
+        for rule in rules:
+        if rule in[ x_rules, vaf_rules]:
+        text = rule(text, vaf)
+        elif rule ==gj_rules:
+        text = rule(text, vvf)
+        else:
+        text = rule(text)
+        if debug:
+        print rule.func_name + ' => ' + text
+        */
+        return text;
+    }
+
     public static String epa(String textoEspanyol, char charCedilla, char charJejeo, boolean escapeLinks, boolean debug)
     {
 
@@ -28,45 +43,25 @@ public class Andaluh {
         exception_rules,
         word_interaction_rules
     ]
+*/
 
-    def transliterate(text):
-        for rule in rules:
-            if rule in [x_rules, vaf_rules]:
-                text = rule(text, vaf)
-            elif rule == gj_rules:
-                text = rule(text, vvf)
-            else:
-                text = rule(text)
-            if debug: print rule.func_name + ' => ' + text
+        String[] tags;
+        if (escapeLinks)
+        {
+            // TODO: idea generar una estructura de datos que sea un array de pares string y boolean solo con una llamada
+            //       para esto hay que sobrecargar
+            // por ejemplo: ParStringBoolean[] textoEspanyolUTF8_Ignorando = ProcessIgnorar(textoEspanyolUTF8);
 
-        return text
-
-    if escapeLinks:
-        ignore = to_ignore_re.findall(text) # Words in the message not to transliterate
-        words = to_ignore_re.split(text) # Spanish words in the message to transliterate
-
-        if not ignore:
-            tags = []
-            text = text
-        else:
-            # Replace words to ignore in the transliteration with randints
-            tags = zip(map(lambda x: str(random.randint(1,999999999)), ignore), ignore)
-            text = ''.join(reduce(lambda x, y: ''.join(x) + ''.join(y), zip(words, [x[0] for x in tags])))
-            if len(words) > len(ignore): text += words[-1]
-
-        if debug: print 'escapeLinks => ' + text
-        text_and = transliterate(text)
-        for tag in tags: text_and = text_and.replace(tag[0], tag[1])
-        if debug: print 'unEscapeLinks => ' + text_and
-        return text_and
-    else:
-        return transliterate(text)
-        */
+            String text_and = transliterate(textoEspanyolUTF8); // aqui iria el textoEspanyolUTF8_Ignorando
 
 
-        // TODO
-        String textoAndaluh = textoEspanyolUTF8;
-        return textoAndaluh;
+            return text_and;
+        }
+        else
+        {
+            return transliterate(textoEspanyolUTF8);
+        }
+
     }
     
     
